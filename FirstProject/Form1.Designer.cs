81b1947b71a6ebb8ac26c80ebe96dac50f312a73
Hanaa00 <97117151+Hanaa00@users.noreply.github.com>
2023-03-15 14:58:10 +0100

    HTML parsing

--- FirstProject/Form1.Designer.cs
+++ FirstProject/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.fileContentTextBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -50,11 +51,21 @@
             this.fileContentTextBox.Size = new System.Drawing.Size(403, 391);
             this.fileContentTextBox.TabIndex = 1;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(646, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Upload HTML file";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.fileContentTextBox);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -68,5 +79,6 @@
 
         private Button button1;
         private TextBox fileContentTextBox;
+        private Button button2;
     }
 }
--- FirstProject/Form1.cs
+++ FirstProject/Form1.cs
@@ -14,25 +14,31 @@ namespace FirstProject
 
         private void button_click(object sender, EventArgs e)
         {
-                OpenFileDialog ofd = new OpenFileDialog();
-                ofd.Filter = "JSON Files (*.json)|*.json|XML Files (*.xml)|*.xml";
-                if (ofd.ShowDialog() == DialogResult.OK)
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "JSON Files (*.json)|*.json|XML Files (*.xml)|*.xml|HTML Files (*.html;*.htm)|*.html;*.htm";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string extension = Path.GetExtension(ofd.FileName);
+                if (extension == ".json")
                 {
-                    string extension = Path.GetExtension(ofd.FileName);
-                    if (extension == ".json")
-                    {
-                        fileContentTextBox.Text = ParseJSONfile.ParseDevice(ofd.FileName);
-                    }
-                    else if (extension == ".xml")
-                    {
-                        fileContentTextBox.Text = ParseXMLfile.ParseXmlFile(ofd.FileName);
-                    }
-                    else
-                    {
-                        MessageBox.Show("Invalid file type. Please select a JSON or XML file.");
-                    }
+                    fileContentTextBox.Text = ParseJSONfile.ParseDevice(ofd.FileName);
+                }
+                else if (extension == ".xml")
+                {
+                    fileContentTextBox.Text = ParseXMLfile.ParseXmlFile(ofd.FileName);
+                }
+                else if (extension == ".html" || extension == ".htm")
+                {
+                    fileContentTextBox.Text = ParseHTMLFile.Parse(ofd.FileName);
+                }
+                else
+                {
+                    MessageBox.Show("Invalid file type. Please select a JSON, XML, or HTML file.");
                 }
             }
         }
     }
+}
+        
+    
 
